d302379225747c5231f2e753ad0adf9859d0b7b7
flasevalice <55132480+flasevalice@users.noreply.github.com>
2024-10-17 21:11:42 +0300

    Введение в Java и JUnit - Тест 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -12,8 +12,17 @@ public class MainClassTest {
     @Test
     public void testGetClassNumber() {
         MainClass mainClass = new MainClass();
-        if (mainClass.getClassNumber()>45)
+        if (mainClass.getClassNumber() > 45)
             System.out.println("Вернулось число больше 45");
         else System.out.println("Вернулось число меньше или равно 45");
+    }
+
+    @Test
+    public void testGetClassString() {
+        MainClass mainClass = new MainClass();
+        if (mainClass.getClassString().contains("hello") ||
+                mainClass.getClassString().contains("Hello")) {
+            System.out.println("Строка содержит hello или Hello");
+        } else System.out.println("Строка НЕ содержит hello или Hello");
     }
 }
